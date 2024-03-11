8d80f8851fc7261302b9876e45e97693e66c78b5
WilkPrintes <wilkp90@gmail.com>
2024-03-11 12:46:28 -0300

    fix init.sql

--- services/postgress/conf/init.sql
+++ services/postgress/conf/init.sql
@@ -1,8 +1,8 @@
 -- Cria o banco de dados bd_odoo (se ainda não existir)
-CREATE DATABASE IF bd_odoo
+CREATE DATABASE bd_odoo
 
 -- Cria o usuário odoo_admin (se ainda não existir) e define a senha
-CREATE USER IF odoo_admin WITH PASSWORD 'odoo_admin'
+CREATE USER odoo_admin WITH PASSWORD 'odoo_admin'
 
 -- Atribui permissões ao usuário para o banco de dados bd_odoo
 GRANT ALL PRIVILEGES ON DATABASE bd_odoo TO odoo_admin
