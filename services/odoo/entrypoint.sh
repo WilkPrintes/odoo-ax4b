67dff5d72b9e23167224836e312c2f582354ed06
WilkPrintes <wilkp90@gmail.com>
2024-03-11 08:00:14 -0300

    socket error

---
+++ services/odoo/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Função para esperar o servidor PostgreSQL estar pronto
+wait_for_db() {
+    host="$1"
+    port="$2"
+    
+    until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$host" -p "$port" -U "$POSTGRES_USER" -c '\q'; do
+        >&2 echo "Servidor PostgreSQL não está pronto para aceitar conexões - esperando..."
+        sleep 1
+    done
+    
+    >&2 echo "Servidor PostgreSQL está pronto para aceitar conexões"
+}
+
+# Espera até que o servidor PostgreSQL esteja pronto para aceitar conexões
+wait_for_db localhost 5432
+
+# Atualiza todos os módulos do Odoo
+echo "Atualizando todos os módulos do Odoo..."
+odoo -c /etc/odoo/odoo.conf -u all
+
+# Inicia o Odoo
+exec "$@"
--- services/postgress/conf/init.sql
+++ services/postgress/conf/init.sql
@@ -3,6 +3,8 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'bd_odoo') THEN
         CREATE DATABASE bd_odoo;
+    ELSE
+        RAISE NOTICE 'O banco de dados bd_odoo já existe, pulando a criação.';
     END IF;
 END $$;
 
@@ -11,6 +13,8 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_user WHERE usename = 'odoo_admin') THEN
         CREATE USER odoo_admin WITH PASSWORD 'odoo_admin';
+    ELSE
+        RAISE NOTICE 'O usuário odoo_admin já existe, pulando a criação.';
     END IF;
 END $$;
 
