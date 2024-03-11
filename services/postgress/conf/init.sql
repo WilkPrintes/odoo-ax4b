9d4d0a13f7d255e4253c6c5dec78779b5a71f3a3
WilkPrintes <wilkp90@gmail.com>
2024-03-11 13:31:23 -0300

    one more ago

--- services/postgress/conf/init.sql
+++ services/postgress/conf/init.sql
@@ -2,7 +2,12 @@
 CREATE DATABASE IF NOT EXISTS bd_odoo;
 
 -- Verifica se o usuário existe antes de tentar criar
-CREATE USER IF NOT EXISTS odoo_admin WITH PASSWORD odoo_admin;
+DO $$ 
+BEGIN 
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_user WHERE usename = 'odoo_admin') THEN
+        CREATE USER odoo_admin WITH PASSWORD 'odoo_admin';
+    END IF;
+END $$;
 
 -- Atribui permissões ao usuário para o banco de dados
 GRANT ALL PRIVILEGES ON DATABASE bd_odoo TO odoo_admin;
