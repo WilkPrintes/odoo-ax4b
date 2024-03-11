e1d6b24015f7b01e665e6624c19d9c2be177e470
WilkPrintes <wilkp90@gmail.com>
2024-03-11 13:37:10 -0300

    i belive

--- services/postgress/conf/init.sql
+++ services/postgress/conf/init.sql
@@ -1,13 +1,4 @@
 -- Verifica se o banco de dados existe antes de tentar criar
-CREATE DATABASE IF NOT EXISTS bd_odoo;
-
--- Verifica se o usuário existe antes de tentar criar
-DO $$ 
-BEGIN 
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_user WHERE usename = 'odoo_admin') THEN
-        CREATE USER odoo_admin WITH PASSWORD 'odoo_admin';
-    END IF;
-END $$;
-
--- Atribui permissões ao usuário para o banco de dados
+CREATE USER odoo_admin
+CREATE DATABASE db_odoo;
 GRANT ALL PRIVILEGES ON DATABASE bd_odoo TO odoo_admin;
